f3a13a4c921a5836daceee3bbecaa85f654f8c8b
Tuholy2004 <Tuholy@outlook.com>
2021-11-11 17:25:53 +0100

    Forbedring på person og lagt til getter

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,7 +4,18 @@ namespace SkuleAdmDemoWeb.Models
 {
     public class Person
     {
-        String alder;
-        String kjonn;
+        Kjonn kjonn;
+        String namn;
+        DateTime bursdag;
+        public enum Kjonn{
+            Kvinne=0,
+            Mann=1
+        }
+        public String GetNamn()
+        {return namn;}
+        public DateTime GetBursdag()
+        {return bursdag;}
+        public Kjonn GetKjonn()
+        {return kjonn;}
     }
 }
