2549a30853db375fc5089fbdb164886f64ada232
Jonathan Kjorlaug <jonathan@kjorlaug.no>
2021-11-11 17:22:29 +0100

    Alder og Fødselsdato!

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,7 +4,22 @@ namespace SkuleAdmDemoWeb.Models
 {
     public class Person
     {
-        String alder;
+        String fornamn;
+        String etternamn;
+        String mellomnanm;
+
+        String visningsnamn ( String fornamn, String etternamn, String mellomnamn ) {
+            return fornamn + etternamn + mellomnanm;
+        }
+
+        DateTime fodselsdato;
+
+        public int alder (){
+            TimeSpan a = DateTime.Now.Subtract(fodselsdato);
+            DateTime d = new DateTime(a.Ticks);
+            return d.Year-1;
+        }
+
         String kjonn;
     }
 }
